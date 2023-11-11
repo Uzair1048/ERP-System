65e193e9147da16161d5f80edc7743bb2e01d72e
uzairulhassan <uzairulhassan@gidsol.pk>
2023-11-11 22:51:51 +0500

    xml added upto signup activity

---
+++ app/src/main/java/com/example/erpsystem/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.erpsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+
+        val btnSignup = findViewById<Button>(R.id.btnSignup)
+        btnSignup.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
